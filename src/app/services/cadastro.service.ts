233e2e83e2c455c074c6223db9f7b493009594b5
Diego Kuwahara de Castro <diegookc@hotmail.com>
2018-11-09 00:05:25 -0200

    Adição do teste de POST do cadastro

--- src/app/services/cadastro.service.ts
+++ src/app/services/cadastro.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root'
-})
+import { Http } from '@angular/http';
+@Injectable()
 export class CadastroService {
 
-    constructor() {
+    private url = "http://localhost:8080/LivrariaVirtual/servicos/Usuario";
 
+    constructor(private http: Http) {}
+
+    insereUsuario(usuarios: any[]){
+        return this.http.post(this.url,usuarios)
     }
 }
