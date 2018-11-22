e4790f3c5bd50899602e88d339b425a0a169d702
Rodrigo Antunes <ro_sgca@hotmail.com>
2018-11-22 19:55:30 -0200

    resolvido

--- src/modelo/livro.ts
+++ src/modelo/livro.ts
@@ -1,12 +1,13 @@
 export class Livro{
+    public id: Number;
+        public nome: string;
+        public autor: string; 
+        public tipo: string;
+        public preco: Number; 
+        public editora: string;
+        public edicao: boolean;
+        public isExcluido: boolean
     constructor(
-        public id: Number = 0,
-        public nome: string = '', 
-        public autor: string = '', 
-        public tipo: string = '',
-        public preco: Number = 0, 
-        public editora: string = '',
-        public edicao: boolean = true,
-        public isExcluido: boolean = false
+        
     ) {}
 }
